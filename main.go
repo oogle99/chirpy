de514799947c0e98aa7881995b4c3ac8a417ca74
oogle99 <jjogle99@gmail.com>
2026-08-10 00:23:20 -0700

    POST requests and profanity filter

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
 
 func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -11,14 +26,92 @@ func handlerReadiness(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("OK"))
 }
 
+func (cfg *apiConfig) handlerFileserverHits(w http.ResponseWriter, _ *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	msg := fmt.Sprintf(`
+		<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+		</html>`, hits)
+	w.Write([]byte(msg))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+}
+
+func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
+	defer req.Body.Close()
+
+	type chirp struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	c := chirp{}
+	err := decoder.Decode(&c)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	if len([]rune(c.Body)) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	bodyCopy := replaceProfane(c.Body)
+
+	respondWithJSON(w, 200, map[string]string{"cleaned_body": bodyCopy})
+}
+
+func replaceProfane(msg string) string {
+	profanity := []string{"kerfuffle", "sharbert", "fornax"}
+
+	splitMsg := strings.Split(msg, " ")
+	for idx, word := range splitMsg {
+		for _, profaneWord := range profanity {
+			loweredWord := strings.ToLower(word)
+			if loweredWord == profaneWord {
+				splitMsg[idx] = "****"
+			}
+		}
+	}
+
+	return strings.Join(splitMsg, " ")
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(code)
+	w.Write(response)
+	return nil
+}
+
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
 
-	mux := &http.ServeMux{}
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	apiCfg := apiConfig{}
 
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux := &http.ServeMux{}
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerFileserverHits)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
